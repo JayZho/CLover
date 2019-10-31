da20a7200b986dbf837a7eb5b2329b588e8c59d2
Yue Wu <may@Yues-MacBook-Pro-4.local>
2019-10-31 11:53:14 +1100

    request class created

---
+++ backend/Request.py
@@ -0,0 +1,25 @@
+
+class Rquest:
+
+    def __init__(self, bloodType, location, notes)
+        self._type = bloodType
+        self._location = location
+        self._notes = notes
+
+    def getType(self):
+        return self._type
+
+    def getLocation(self):
+        return self._location
+
+    def getNotes(self):
+        return self._notes
+
+    def setType(self, bloodType):
+        self._type = bloodType
+
+    def getLocation(self, location):
+        self._location = location
+
+    def getxotes(self, notes):
+        self.notes = notes
